e0cd212319962665f0f60ad19e02a30b7ee2628f
lidya demerw <demerwlidya@gmail.com>
2025-08-04 16:38:39 -0700

    Clean start without large files

---
+++ 01.js
@@ -0,0 +1,12 @@
+// let myage=35
+// let humandogration= myage * 7
+// // 1 human year = 7 dog years
+// console.log(humandogration)
+
+let bounusepoint=50
+bounusepoint +=50
+console.log(bounusepoint)
+bounusepoint -=75
+console.log(bounusepoint)
+bounusepoint+=45
+console.log(bounusepoint)
