403f5f70f27e288c7cc2b0d10937182be673688f
lidya demerw <demerwlidya@gmail.com>
2025-11-07 13:47:47 -0800

    Align title to center

--- script/01.js
+++ script/01.js
@@ -3,10 +3,14 @@
 // // 1 human year = 7 dog years
 // console.log(humandogration)
 
-let bounusepoint=50
+let bounusepoint=50 
+hi
 bounusepoint +=50
-console.log(bounusepoint)
+console.log(bounusepoin
 bounusepoint -=75
 console.log(bounusepoint)
 bounusepoint+=45
 console.log(bounusepoint)
+//window.alert("this is a window alert")
+// comment
+document.getByid("hs").textcontent="hello world";
